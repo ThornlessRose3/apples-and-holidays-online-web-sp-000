d32583aac91329862f2e7d4f07c310fca8ea7452
Carrie Jones <c.jessica.jones@gmail.com>
2020-06-20 18:25:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, data|
     puts season.to_s.capitalize + ":"
      data.each do |holiday, list|
-       holiday_string = holiday.humanize
+       holiday_string = holiday.capitalize
+       holiday_string = holiday_string.humanize
        # if holiday_string.
        # temp = holiday_string.split('_')
        # holiday_string = temp.join(" ")
