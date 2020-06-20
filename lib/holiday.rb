38b9be3db765c195cd239fa3733c0c830cdec542
Carrie Jones <c.jessica.jones@gmail.com>
2020-06-20 18:08:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
        list_string = ""
        i = 0
        while list.length > i
-        list_string = list_string + list[i].to_s + 
+        list_string = list_string + list[i].to_s 
         i += 1
         if list.length > i
           list_string = list_string + ", "
