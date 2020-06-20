8c258950f93a44df834e4096075fefa6aacdd67d
Carrie Jones <c.jessica.jones@gmail.com>
2020-06-20 18:47:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,21 +58,19 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, data|
     puts season.to_s.capitalize + ":"
      data.each do |holiday, list|
+       # holiday string
        holiday_string = holiday.to_s
        holiday_string = holiday_string.split('_').map(&:capitalize).join(' ')
-       
-       # holiday_string = holiday_string.gsub(/[_]/, ' ')
-       # holiday_string = holiday_string.capitalize
        # list string
-       list_string = ""
-       i = 0
-       while list.length > i
-        list_string = list_string + list[i].to_s 
-        i += 1
-        if list.length > i
-          list_string = list_string + ", "
-        end
-       end
+       list_string = list.map
+       # i = 0
+       # while list.length > i
+        # list_string = list_string + list[i].to_s 
+        # i += 1
+        # if list.length > i
+        #  list_string = list_string + ", "
+        # end
+       # end
        puts "  " + holiday_string + ": " + list_string
       # day = holiday[0].to_s.capitalize + ": "
       # supplies = holiday.to_s
